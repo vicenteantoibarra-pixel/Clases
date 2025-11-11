eb311c83a85c4d630f36b0cf085ed5d46a3f5dc8
Vicente Ibrra <vicenteantoibarra@gmail.com>
2025-11-10 21:13:11 -0300

    validacion con RE

--- Clase10-11-25/clase/persona.py
+++ Clase10-11-25/clase/persona.py
@@ -1,16 +1,26 @@
+import re
+
 class Persona:
-    def __init__(self,nombre,apellido,telefono):
+    def __init__(self,nombre,apellido,telefono,correo):
         self.__nombre = nombre
         self.__apellido = apellido
         self.__telefono = telefono
-    
+        self.__correo = correo
+        
     def getNombre(self):
         return f"{self.__nombre} {self.__apellido} "
     
     def getTelefono (self):
         return f"{self.__telefono}"
     
+    def getCorreo (self):
+        return self.__correo  
     
-    
+    def setCorreo (self,nuevo_correo):
+        patron_correo = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        valido =re.match (patron_correo,nuevo_correo)
+        if valido:
+            self.__correo = nuevo_correo
+       
 
         
--- Clase10-11-25/main.py
+++ Clase10-11-25/main.py
@@ -1,7 +1,11 @@
 from clase.persona import Persona
 
-persona = Persona("Vicente","Ibarra","+56936833638")
+persona = Persona("Vicente","Ibarra","+56936833638","Inacap@gmail.cl")
 
 print(persona.getNombre())
 
 print(persona.getTelefono())
+
+print(persona.getCorreo())
+
+persona.setCorreo("micorreo@miratu.cl")
