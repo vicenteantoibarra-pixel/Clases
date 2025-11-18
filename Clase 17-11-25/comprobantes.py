a86320d7d06497483a3920085adaa7d9bddda964
Vicente Ibrra <vicenteantoibarra@gmail.com>
2025-11-17 21:30:09 -0300

    clase del dia 17

---
+++ Clase 17-11-25/comprobantes.py
@@ -0,0 +1,15 @@
+
+class Documento:
+    def __init__(self, fecha, id):
+        self.__id = id
+        self.__fecha = fecha
+
+    @property
+    def id (self):
+        return self.__id
+    
+    @id.setter
+    def id (self, id):
+        if id  <0:
+            raise Exception ("El ID debe ser mayor a '0' ")
+        self.__id = id 
---
+++ Clase 17-11-25/main.py
@@ -0,0 +1,6 @@
+from comprobantes import Documento
+
+documento = Documento ("2025-11-17", 12)
+
+print(documento.id)
+
